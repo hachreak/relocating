15bfc665f33f42d8800d1d463f16fc1ed0ad4909
Leonardo Rossi <leonardo.r@cern.ch>
2017-10-04 17:18:35 +0200

    init repo
    Signed-off-by: Leonardo Rossi <leonardo.r@cern.ch>

---
+++ README.md
@@ -0,0 +1,9 @@
+relocating
+==========
+
+An OTP application
+
+Build
+-----
+
+    $ rebar3 compile
---
+++ src/relocating_sup.erl
@@ -0,0 +1,37 @@
+%%%-------------------------------------------------------------------
+%% @doc relocating top level supervisor.
+%% @end
+%%%-------------------------------------------------------------------
+
+-module(relocating_sup).
+
+-author('Leonardo Rossi <leonardo.rossi@studenti.unipr.it>').
+
+-behaviour(supervisor).
+
+%% API
+-export([start_link/0]).
+
+%% Supervisor callbacks
+-export([init/1]).
+
+-define(SERVER, ?MODULE).
+
+%%====================================================================
+%% API functions
+%%====================================================================
+
+start_link() ->
+    supervisor:start_link({local, ?SERVER}, ?MODULE, []).
+
+%%====================================================================
+%% Supervisor callbacks
+%%====================================================================
+
+%% Child :: {Id,StartFunc,Restart,Shutdown,Type,Modules}
+init([]) ->
+    {ok, { {one_for_all, 0, 1}, []} }.
+
+%%====================================================================
+%% Internal functions
+%%====================================================================
