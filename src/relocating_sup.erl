7ceb8918922d74897016530e7f92d1320069837b
Leonardo Rossi <leonardo.r@cern.ch>
2017-10-12 16:07:38 +0200

    sup: one_for_one
    Signed-off-by: Leonardo Rossi <leonardo.r@cern.ch>

--- src/relocating_sup.erl
+++ src/relocating_sup.erl
@@ -30,7 +30,7 @@ start_link() ->
 
 %% Child :: {Id,StartFunc,Restart,Shutdown,Type,Modules}
 init([]) ->
-    {ok, { {one_for_all, 0, 1}, []} }.
+    {ok, { {one_for_one, 10, 10}, []} }.
 
 %%====================================================================
 %% Internal functions
